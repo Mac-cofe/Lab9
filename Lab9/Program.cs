01c3d55cf30e6d3720fceffa2bf7fe78dd61a8dd
M.M.Karmanova@urfu.me <M.M.Karmanova@urfu.me>
2021-10-10 22:18:33 +0500

    Корректировка

--- Lab9/Program.cs
+++ Lab9/Program.cs
@@ -10,45 +10,67 @@ namespace Lab9
     {
         static void Main(string[] args)
         {
+            int x = 0; int y = 0;
             Console.WriteLine("Вас приветствует Калькулятор 1.0");
             Console.WriteLine("Я пока только умею складывать, вычитать, умножать и делить числа");
+            Console.WriteLine("--------------------------------------------------------------");
 
-            Console.Write("Введите целое число");
-            int x = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Введите целое число");
-            int y = Convert.ToInt32(Console.ReadLine());
-
+            Console.Write("Введите первое целое число  ");
+            while (!int.TryParse(Console.ReadLine(), out x))                                     // здесь не получилось именно через try, catch
+            {
+                Console.Write("   Это должно быть целое число!\n     Повторите ввод: ");
+            }
+            Console.WriteLine();
+            Console.Write("Введите второе целое число  ");
+            while (!int.TryParse(Console.ReadLine(), out y))
+            {
+                Console.Write("   Это должно быть целое число!\n     Повторите ввод: ");
+            }
+            Console.WriteLine("--------------------------------------------------------------");
             Console.WriteLine("Выберите код операции:");
             Console.WriteLine("1 - сложение");
             Console.WriteLine("2 - вычитание");
             Console.WriteLine("3 - умножение");
             Console.WriteLine("4 - деление");
+            Console.WriteLine("--------------------------------------------------------------");
+            Console.Write("Ваш выбор:  ");
             int operation = Convert.ToInt32(Console.ReadLine());
-
             switch (operation)
-             case 1:
-                {
-                    break;
-                }
-            case 2:
-                {
-                    break;
-                }
-            case 3:
-                {
-                    break;
-                }
-            case 4:
-                {
-                    break;
-                }
-            default:
-                {
-                    Console.WriteLine("Такую операцию я делать не умею");
-                    break;
-                }
-
-
+            {
+                case 1:
+                    {
+                        Console.WriteLine("{0} + {1} = {2}", x, y, x+y);
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.WriteLine("{0} - {1} = {2}", x, y, x-y);
+                        break;
+                    }
+                case 3:
+                    {
+                        Console.WriteLine("{0} * {1} = {2}", x, y, x*y);
+                        break;
+                    }
+                case 4:
+                    {
+                        try
+                        {
+                            Console.WriteLine("{0} / {1} = {2,3:f2}", x, y, x / y);  // при такой записи исключение обрабатывается, но ответ может быть вещественным. И если исправить на (double)x / y  или  Convert.ToDouble(x / y), то исключение не обрабатывается
+                        }
+                        catch (Exception ex) when (y == 0)              // обработка исключения
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Выбрана некорректная операция");
+                        break;
+                    }
             }
+            Console.ReadKey();
+        }
     }
 }
---
+++ Lab9_Calc_ver2/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int otvet;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine();
+                Console.WriteLine("Вас приветствует Калькулятор 1.0");
+                Console.WriteLine("Я пока только умею складывать, вычитать, умножать и делить числа");
+                Console.WriteLine("--------------------------------------------------------------");
+                int x = 0; int y = 0;
+                Console.Write("Введите первое целое число  ");
+                while (!int.TryParse(Console.ReadLine(), out x))                                     // здесь не получилось именно через try, catch
+                {
+                    Console.Write("   Это должно быть целое число!\n     Повторите ввод: ");
+                }
+                Console.WriteLine();
+                Console.Write("Введите второе целое число  ");
+                while (!int.TryParse(Console.ReadLine(), out y))
+                {
+                    Console.Write("   Это должно быть целое число!\n     Повторите ввод: ");
+                }
+                Console.WriteLine();
+                Console.WriteLine("Выберите операцию: +  -  *  / ");
+                Console.WriteLine("--------------------------------------------------------------");
+                Console.WriteLine();
+                Console.Write("Ваш выбор:  ");
+                char operation = Convert.ToChar(Console.ReadLine());
+                switch (operation)
+                {
+                    case '+':
+                        {
+                            Console.WriteLine("Результат: {0} + {1} = {2}", x, y, x + y);
+                            break;
+                        }
+                    case '-':
+                        {
+                            Console.WriteLine("Результат: {0} - {1} = {2}", x, y, x - y);
+                            break;
+                        }
+                    case '*':
+                        {
+                            Console.WriteLine("Результат: {0} * {1} = {2}", x, y, x * y);
+                            break;
+                        }
+                    case '/':
+                        {
+                            if (y != 0)
+                            {
+                                Console.WriteLine("Результат: {0} / {1} = {2,3:f2}", x, y, (double)x / y);    //проверка через if
+                            }
+                            else
+                            {
+                                Console.WriteLine("Ошибка деления на ноль");
+                            }
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Выбрана некорректная операция");
+                            break;
+                        }
+                }
+                Console.WriteLine();
+                Console.WriteLine("Решим другой пример? 1 - да, 0 - нет");
+                Console.Write("Ваш ответ:");
+                otvet = Convert.ToInt32(Console.ReadLine());
+
+            }
+            while (otvet == 1);
+            Console.WriteLine();
+            Console.WriteLine("Для закрытия окна приложения нажмите Enter");
+            Console.ReadKey();
+        }
+    }
+}
